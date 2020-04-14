4922014474e0907181a4a16faa1ec1d3340e46ae
KangSehui <21600005@handong.edu>
2020-04-14 03:56:42 -0700

    hw2 done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 int sorted[10];
 
 int main(){
+    
+    int data[10]; 
 
+    for(int i=0; i<10; i++){ 
+        cin >> data[i]; 
+    } 
+
+    merge_sort(data, 0, 9); 
+
+    // 결과 확인 
+    for(int i=0; i<10; i++){ 
+        cout << data[i] << " "; 
+    } 
+    cout << endl;
     return 0;
 }
