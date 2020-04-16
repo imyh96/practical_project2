875c80caaeef951db6a15d4c6bef2aaf6ee92eff
imyh96 <21600855@handong.edu>
2020-04-16 02:13:57 -0700

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include "sorting.h"
 
+void merge_sort(int *data, int start, int end);
+
 int sorted[10];
+int my_data[10];
 
 int main(){
+    int i = 0;
+
+    for(i = 1; i < 11; i++){
+        cout << "input " << i << " number: ";
+        cin >> my_data[i-1];
+    }
+
+    merge_sort(my_data, 0, 10);
+    
+    for(i = 0; i < 10; i++){
+        cout <<  sorted[i] << " ";
+    }
 
     return 0;
 }
