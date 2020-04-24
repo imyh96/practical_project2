e6beae2e7984e1d3bc28192ddc7afe1cb41df5ee
KangSehui <21600005@handong.edu>
2020-04-24 01:41:07 -0700

    new hw3

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,13 @@
 #include "calculator.h"
 
 void addition(){
-
+    int answer = a + b;
+    cout << "= " << answer << endl;
 }
 
 void subtraction(){
-
+    int answer = a - b;
+    cout << "= " << answer << endl;
 }
 
 void multiplication(){
